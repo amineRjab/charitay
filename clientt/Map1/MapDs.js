cc8a15131a6f715d656da3d325cbc9d15cd55dc1
amineRjab <aminerjab93@gmail.com>
2023-03-16 18:51:00 +0100

    test

--- clientt/Map1/MapDs.js
+++ clientt/Map1/MapDs.js
@@ -213,7 +213,7 @@ const Map = () => {
           <MapViewDirections
             origin={pin}
             destination={destination}
-            apikey={"AIzaSyB3gw78dU8-sOg2nzSiHi4-7LUgEedSasM"}
+            apikey={""}
             strokeWidth={5}
             strokeColor="#0096FF"
           />
@@ -300,7 +300,7 @@ const Map = () => {
                     <MapViewDirections
                       origin={pin}
                       destination={destination}
-                      apikey={"AIzaSyB3gw78dU8-sOg2nzSiHi4-7LUgEedSasM"}
+                      apikey={""}
                       strokeWidth={5}
                       strokeColor="#0096FF"
                     />
